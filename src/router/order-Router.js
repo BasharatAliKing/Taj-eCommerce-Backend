aa93496bce803b7e91e7ed17d6348c74861ae907
Basharat Ali <80580890+BasharatAliKing@users.noreply.github.com>
2025-09-04 19:13:23 +0500

    update

--- src/router/order-Router.js
+++
@@ -1,7 +0,0 @@
-const express=require("express");
-const router=express.Router();
-const OrderController=require("../controller/order-Controller");
-
-router.route('/order-food').post(OrderController.createOrder);
-
-module.exports=router;
