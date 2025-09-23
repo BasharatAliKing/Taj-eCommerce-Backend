3e13f4009cd381e417e682c9d183ae3833e05210
Basharat Ali <80580890+BasharatAliKing@users.noreply.github.com>
2025-09-23 18:27:35 +0500

    update

--- src/server.js
+++ src/server.js
@@ -34,7 +34,7 @@ app.use(cors({
 app.use(express.static("public"));
 // Increase JSON payload limit
 app.use(express.json({ limit: "20mb" }));
-
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 // app.use(express.json());
 
 app.get('/',(req,res)=>{
