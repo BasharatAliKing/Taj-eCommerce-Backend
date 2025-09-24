c8d304f845ea5f70e563e1554f32daa559ba128d
Basharat Ali <80580890+BasharatAliKing@users.noreply.github.com>
2025-09-24 11:24:12 +0500

    update

--- src/controller/user-Controller.js
+++ src/controller/user-Controller.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const User = require("../models/user-model");
 const bcrypt = require("bcryptjs");
+const crypto = require("crypto");
 const sendEmailToMultipleUsers=require("../utils/forgotPasswordEmail")
 const Register = async (req, res) => {
   try {
